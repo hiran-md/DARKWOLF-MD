8a6092eb2bc4f2aa8c268abf0ef432c6faa849ec
Hiranya Sathsara <hiranmdbot@gmail.com>
2025-01-22 20:03:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ let up = `
 > © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜɪʀᴜᴡᴀ
 *•────────────•⟢*
 `;
-conn.sendMessage(conn.user.id, { image: { url: `https://raw.githubusercontent.com/hiran-md/Darkwolf-DATA_BASE/refs/heads/main/Logo/DARK_WOLF-MD.webp?token=GHSAT0AAAAAAC5GPW4IHCFRM4UFAVJVHRWAZ4Q7RLA` }, caption: up })
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url:'https://raw.githubusercontent.com/hiran-md/Darkwolf-DATA_BASE/refs/heads/main/Logo/DARK_WOLF-MD.webp?token=GHSAT0AAAAAAC5GPW4IHCFRM4UFAVJVHRWAZ4Q7RLA'}, caption: up })
+conn.sendMessage(conn.user.id, { image: { url: `https://raw.githubusercontent.com/hiran-md/Darkwolf-DATA_BASE/refs/heads/main/Logo/DARK_WOLF-MD.webp` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url:'https://raw.githubusercontent.com/hiran-md/Darkwolf-DATA_BASE/refs/heads/main/Logo/DARK_WOLF-MD.webp'}, caption: up })
 }
 })
 conn.ev.on('creds.update', saveCreds)  
